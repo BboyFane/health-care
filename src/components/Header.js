5abf5c868132edb27eeb9138fa3786e0acf619d8
BboyFane <hua.stephane@gmail.com>
2021-10-11 22:42:29 +0200

    feat(header): add buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import headerBackground from '../assets/header-background.svg'
+import { Button } from '@mui/material'
 
 function Header() {
     return (
@@ -8,9 +9,15 @@ function Header() {
             <header style={{minWidth: '100%', minHeight: '900px', background: `no-repeat center 100% url(${headerBackground})`, backgroundSize: 'cover'}}>
                 <Navbar/>
                 <div className='header-left-corner' style={{width: '40%', padding: 80, marginTop: 50, position: 'center'}}>
-                    <h2>Stay Strong, Live long.</h2>
-                    <h1><em>Take care</em> of your body and it will take care of <em>you</em>.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit feugiat viverra est suspendisse porta tortor commodo, tellus. Neque,</p>
+                    <div>
+                        <h2>Stay Strong, Live long.</h2>
+                        <h1><em>Take care</em> of your body and it will take care of <em>you</em>.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit feugiat viverra est suspendisse porta tortor commodo, tellus. Neque,</p>
+                    </div>
+                    <Button variant="contained">Get Appointment</Button>
+                    {'  '}
+                    {/* <span>&nbsp;&nbsp;</span> */}
+                    <Button variant="outlined">View Profile</Button>
                 </div>
             </header>
         </div>
