c8f85a25a3a01664ffe70d2d073283291ff65d18
BboyFane <hua.stephane@gmail.com>
2021-10-13 20:00:22 +0200

    feat(structure): dev not done because of computer latency, missing end of front, form and hosting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import { Button } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 
 function Header() {
     return (
         <div className='header-section'>
             <header className='header-background'>
                 <Navbar/>
-                <div className='header-left-corner' style={{width: '40%', padding: 80, marginTop: 50, position: 'center'}}>
+                <div className='header-left-corner'>
                     <div>
                         <h3 className='title-shadow'>Stay Strong, Live long.</h3>
                         <h1><em>Take care</em> of your body and it will take care of <em>you</em>.</h1>
                         <p className='header-subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit feugiat viverra est suspendisse porta tortor commodo, tellus. Neque,</p>
                     </div>
-                    <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                        <Button className='cta' variant="contained">Get Appointment</Button>
-                        {'  '}
-                        {/* <span>&nbsp;&nbsp;</span> */}
-                        <Button className='outline-cta' variant="outlined">View Profile</Button>
-                    </div>
+                    <Grid container>
+                        <Grid md={6}>
+                            <Button className='cta' variant="contained">Get Appointment</Button>
+                        </Grid>
+                        <Grid md={6}>
+                            <Button className='outline-cta' variant="outlined">View Profile</Button>
+                        </Grid>
+                    </Grid>
                 </div>
             </header>
         </div>
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,41 +1,49 @@
-import { Grid } from '@mui/material'
+import { Card, Grid } from '@mui/material'
 import React from 'react'
 
 function Skill() {
     return (
         <div className='skill-section' style={{paddingInline: 80}}>
-            <div className='skill-introduction' style={{width: '60%', textAlign: 'center', margin: 'auto'}}>
-                <h2>My Skill depends on work.</h2>
+            <div className='skill-introduction' style={{width: '55%', textAlign: 'center', margin: 'auto'}}>
+                <h2>My Skill <em>depends</em> on work.</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. blandit feugiat viverra est suspendisse porta tortor commodo, tellus. Neque,</p>
             </div>
             <Grid container spacing={2} style={{paddingInline: 10}}>
                 <Grid item md={3} className='skill-safety'>
-                    <div className='skill-safety-logo'>logo</div>
-                    <div className='skill-safety-text'>
-                        <h4>title</h4>
-                        <p>feugiat viverra est suspendis porta tortor commodo</p>
-                    </div>
+                    <Card style={{backgroundColor: '#F25A3C', width: 265, height: 350}}>
+                        <div className='skill-safety-logo'>logo</div>
+                        <div className='skill-safety-text'>
+                            <h4>title</h4>
+                            <p>feugiat viverra est suspendis porta tortor commodo</p>
+                        </div>
+                    </Card>
                 </Grid>
                 <Grid item md={3} className='skill-cared'>
-                    <div className='skill-cared-logo'>logo</div>
-                    <div className='skill-cared-text'>
-                        <h4>title</h4>
-                        <p>feugiat viverra est suspendis porta tortor commodo</p>
-                    </div>
+                    <Card style={{backgroundColor: '#CC7926', width: 265, height: 350}}>
+                        <div className='skill-cared-logo'>logo</div>
+                        <div className='skill-cared-text'>
+                            <h4>title</h4>
+                            <p>feugiat viverra est suspendis porta tortor commodo</p>
+                        </div>
+                    </Card>
                 </Grid>
                 <Grid item md={3} className='skill-service'>
+                    <Card style={{backgroundColor: '#739D1E', width: 265, height: 350}}>
                     <div className='skill-service-logo'>logo</div>
                     <div className='skill-service-text'>
                         <h4>title</h4>
                         <p>feugiat viverra est suspendis porta tortor commodo</p>
                     </div>
+                    </Card>
                 </Grid>
                 <Grid item md={3} className='skill-support'>
+                    <Card style={{backgroundColor: '#20BBA1', width: 265, height: 350}}>
                     <div className='skill-support-logo'>logo</div>
                     <div className='skill-support-text'>
                         <h4>title</h4>
                         <p>feugiat viverra est suspendis porta tortor commodo</p>
                     </div>
+                    </Card>
                 </Grid>
             </Grid>
         </div>
